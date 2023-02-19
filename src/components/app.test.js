81ab98f28e6904cdf75e333c89e8d333099b779e
mallikaag01 <107590568+mallikaag01@users.noreply.github.com>
2023-02-19 10:15:28 -0500

    Add files via upload
    MallikaGupta-final

---
+++ src/components/app.test.js
@@ -0,0 +1,27 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+import App from "./App";
+
+jest.mock("axios");
+
+test("renders learn react link", async () => {
+  axios.get.mockImplementation(() => {
+    return {
+      data: {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+      },
+    };
+  });
+
+  render(<App />);
+
+  expect(screen.getByText(/React App/i)).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getByTestId("title").innerHTML).toBe(
+      "Title: delectus aut autem"
+    );
+  });
+});
